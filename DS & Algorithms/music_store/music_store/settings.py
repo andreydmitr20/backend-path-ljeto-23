6c7e6c8c3cce5966ce2e0ce2bef66ab80b85b80e
andreydmitr21 <andreydmitr20@gmail.com>
2023-03-26 20:19:05 +0200

    Repair Django Secret Key accessible on GitHub

--- DS & Algorithms/music_store/music_store/settings.py
+++ DS & Algorithms/music_store/music_store/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+SECRET_KEY = config(
+    "SECRET_KEY"
+)  # this is to replace the secret key you cut away before
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w^u$297d$-xj$+_%bip^c0xrz-*dq+!*6vvpx#7)2&lnqy*7$8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
